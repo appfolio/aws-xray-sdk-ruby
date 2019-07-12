899967eca17cbd8ccc39b75dd45a1f55fca51c2a
Ke Zhang <ke.zhang@mycase.com>
2019-07-11 17:06:14 -0700

    Add an enable switch for rails middleware

--- lib/aws-xray-sdk/facets/rails/railtie.rb
+++ lib/aws-xray-sdk/facets/rails/railtie.rb
@@ -4,15 +4,17 @@ require 'aws-xray-sdk/facets/rails/ex_middleware'
 module XRay
   # configure X-Ray instrumentation for rails framework
   class Railtie < ::Rails::Railtie
-    RAILS_OPTIONS = %I[active_record].freeze
+    RAILS_OPTIONS = %I[active_record enabled].freeze
 
     initializer("aws-xray-sdk.rack_middleware") do |app|
-      app.middleware.insert 0, Rack::Middleware
-      app.middleware.use XRay::Rails::ExceptionMiddleware
+      if app.config.respond_to?('xray') && app.config.xray[:enabled]
+        app.middleware.insert 0, Rack::Middleware
+        app.middleware.use XRay::Rails::ExceptionMiddleware
+      end
     end
 
     config.after_initialize do |app|
-      if app.config.respond_to?('xray')
+      if app.config.respond_to?('xray') && app.config.xray[:enabled]
         options = app.config.xray
         require 'aws-xray-sdk/facets/rails/active_record' if options[:active_record]
         general_options = options.reject { |k, v| RAILS_OPTIONS.include?(k) }
